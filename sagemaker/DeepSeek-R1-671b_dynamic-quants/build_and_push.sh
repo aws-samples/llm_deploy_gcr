772ee636d3bf037d7c7c819dde8e7b92f4bf33b4
FuChen <fuchen@foxmail.com>
2025-02-07 15:13:48 +0800

    add deepseek 671b

---
+++ sagemaker/DeepSeek-R1-671b_dynamic-quants/build_and_push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+REPO_NAMESPACE=${REPO_NAMESPACE:-"sagemaker_endpoint/llama.cpp"}
+REPO_TAG="server-cuda"
+
+# Get the ACCOUNT and REGION defined in the current configuration (default to us-west-2 if none defined)
+
+ACCOUNT=${ACCOUNT:-$(aws sts get-caller-identity --query Account --output text)}
+REGION=${REGION:-$(aws configure get region)}
+
+# If the repository doesn't exist in ECR, create it.
+aws ecr describe-repositories --repository-names "${REPO_NAMESPACE}" > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+echo "create repository:" "${REPO_NAMESPACE}"
+aws ecr create-repository --repository-name "${REPO_NAMESPACE}" > /dev/null
+fi
+
+# Log into Docker
+if [[ "$REGION" = cn* ]]; then
+    aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com.cn
+    REPO_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com.cn/${REPO_NAMESPACE}:${REPO_TAG}"
+else
+    aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com
+    REPO_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAMESPACE}:${REPO_TAG}"
+fi
+
+echo ${REPO_NAME}
+
+# Build docker
+docker build -t ${REPO_NAMESPACE}:${REPO_TAG} .
+
+# Push it
+docker tag ${REPO_NAMESPACE}:${REPO_TAG} ${REPO_NAME}
+docker push ${REPO_NAME}
